88cac91aec0c6c6179f63251312bcbe6d01bacde
John-Oula <johnoula@icloud.com>
2022-02-11 04:16:34 +0800

    fixed navbar and search

--- Components/NavigationBar.js
+++ Components/NavigationBar.js
@@ -9,6 +9,7 @@ import {
     MenuButton,
     MenuItem,
     MenuList,
+    Spacer,
     Text
 } from '@chakra-ui/react'
 
@@ -31,160 +32,91 @@ const NavigationBar = ({nav}) => {
             bgColor={`#1e9339`}
             w={`100%`} h={`auto`} boxShadow={`lg`}>
 
-            <Container centerContent maxW={ 'container.xl'}>
+            <Container maxW={'container.xl'}>
 
+                <Flex display={"flex"} alignItems={`cnter`} flexDirection={`row`}>
+                    <Navbar collapseOnSelect expand="md" bg="#1e9339" variant="light">
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
 
-                {/*<Flex  display={["none", "none", "flex", "flex", "flex"]} flexDirection={`row`}>*/}
-                {/*    <Link href={`/`}>*/}
-                {/*        <Flex*/}
-                {/*            cursor={`pointer`}*/}
-                {/*            fontSize={`sm`}*/}
-                {/*            borderRadius={`0px`}*/}
-                {/*            // borderColor={`#1e9339`} borderLeftWidth={`0.5px`} borderRightWidth={`0.5px`}*/}
-                {/*            pl={`5`}*/}
-                {/*            pr={`5`} bgColor={`#ffffff`} alignItems={'center'} color={`#ffffff`}*/}
-                {/*        > <AiFillHome /> </Flex></Link>*/}
-
-                {/*    {*/}
-                {/*        nav?.map((each, index) => {*/}
-                {/*            return (*/}
-
-                {/*                each?.dropdown ?*/}
-                {/*                    <Menu key={each?._id + index.toString()}>*/}
-                {/*                        <MenuButton*/}
-
-                {/*                            as={Button} rightIcon={<ChevronDownIcon/>}*/}
-                {/*                            fontSize={`sm`}*/}
-                {/*                            borderRadius={`0px`}*/}
-                {/*                            h={`auto`}*/}
-                {/*                            // borderColor={`#1e9339`} borderLeftWidth={`0.5px`}*/}
-                {/*                            pl={`5`}*/}
-                {/*                            pr={`5`} bgColor={`#ffffff`} alignItems={'center'} color={`#ffffff`}*/}
-                {/*                        >*/}
-                {/*                            <Link href={`/${each?.slug.current}`}>{each?.title}</Link>*/}
-                {/*                        </MenuButton>*/}
-                {/*                        <MenuList>*/}
-
-                {/*                            {*/}
-                {/*                                each?.submenu && each?.submenu.map(link => (<MenuItem key={link._id}><Link*/}
-                {/*                                    href={`/${each?.slug.current}${ each.single ? '' : '/'+link.slug.current}`}>{link.title}</Link></MenuItem>))*/}
-                {/*                            }*/}
-                {/*                        </MenuList>*/}
-                {/*                    </Menu>*/}
-                {/*                    :*/}
-                {/*                    <Flex cursor={`pointer`} key={each?._id + index.toString()} fontSize={`sm`}*/}
-                {/*                          borderRadius={`0px`}*/}
-                {/*                          // borderColor={`#1e9339`} borderLeftWidth={`0.5px`} borderRightWidth={`0.5px`}*/}
-                {/*                          pl={`5`}*/}
-                {/*                          pr={`5`} bgColor={`#ffffff`} alignItems={'center'} color={`#ffffff`}>*/}
-                {/*                        <Link href={`/${each?.slug.current}`}><Text>{each?.title}</Text></Link>*/}
-                {/*                    </Flex>*/}
-
-
-                {/*            )*/}
-                {/*        })*/}
-                {/*    }*/}
-
-                {/*    <Flex p={2}>*/}
-                {/*        <form  onSubmit={(e) => {*/}
-                {/*            e.preventDefault()*/}
-                {/*            history.push({pathname: '/search', query: {text: text}})*/}
-                {/*        }} w={100} ml={10}>*/}
-                {/*            <InputGroup >*/}
-                {/*                <InputLeftElement*/}
-
-                {/*                    pointerEvents='none'*/}
-                {/*                    children={<SearchIcon color='#1e9339'/>}*/}
-                {/*                />*/}
-                {/*                <Input onChange={(e) => setText(e.target.value)} color={`#444444`} type='text'*/}
-                {/*                       bgColor={`#ffffff`} border={`solid`} borderColor={`#1e9339`}*/}
-                {/*                       borderWidth={`2px`}/>*/}
-                {/*            </InputGroup>*/}
-
-                {/*        </form>*/}
-                {/*    </Flex>*/}
-
-                {/*</Flex>*/}
-                <Flex display={"flex"} alignItems={`center`}  flexDirection={`row`}>
-                    <Navbar collapseOnSelect expand="lg" bg="#1e9339" variant="light">
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
-<Nav >
-<Flex justifyContent={`center`} flexDirection={[`column`,`column`,`row`,`row`,`row`,]}>
-    <Link href={`/`}>
-        <Flex
-            cursor={`pointer`}
-            fontSize={`sm`}
-            borderRadius={`0px`}
-            // borderColor={`#1e9339`} borderLeftWidth={`0.5px`} borderRightWidth={`0.5px`}
-            pl={`3`}
-            pr={`3`} bgColor={`#1e9339`} alignItems={'center'} color={`#ffffff`}
-        ><AiFillHome /></Flex></Link>
-    {
-        nav?.map((each, index) => {
-            return (
+                            <Nav>
+                                <Flex
+                                      flexDirection={[`column`, `column`, `row`, `row`, `row`,]}>
+                                    <Link href={`/`}>
+                                        <Flex
+                                            cursor={`pointer`}
+                                            fontSize={`sm`}
+                                            borderRadius={`0px`}
+                                            // borderColor={`#1e9339`} borderLeftWidth={`0.5px`} borderRightWidth={`0.5px`}
+                                            pl={`3`}
+                                            pr={`3`} bgColor={`#1e9339`} alignItems={'center'} color={`#ffffff`}
+                                        ><AiFillHome/></Flex></Link>
+                                    {
+                                        nav?.map((each, index) => {
+                                            return (
 
 
-                <Menu>
+                                                <Menu>
 
-                    <MenuButton key={each?._id + index.toString()} fontSize={`sm`}
-                                borderRadius={`0px`} borderColor={`#1e9339`}
-                        // rightIcon={<ChevronDownIcon/>}
-                                borderLeftWidth={`0.5px`} borderRightWidth={`0.5px`}
-                                pl={[0,0,3,3,5]}
-                                pr={[0,0,3,3,5]}
-                                pt={[3,3,3,3,5]}
-                                pb={[3,3,3,3,5]}
-                        // bgColor={`#0c3344`}
-                                alignItems={'center'}
-                                color={`#ffffff`}>
-                        <Link href={`/${each?.slug?.current}`}>{each?.title}</Link><ChevronDownIcon/>
-                    </MenuButton>
-                    <MenuList>
+                                                    <MenuButton key={each?._id + index.toString()} fontSize={`sm`}
+                                                                borderRadius={`0px`} borderColor={`#1e9339`}
+                                                        // rightIcon={<ChevronDownIcon/>}
+                                                                borderLeftWidth={`0.5px`} borderRightWidth={`0.5px`}
+                                                                pl={[0, 0, 3, 3, 5]}
+                                                                pr={[0, 0, 3, 3, 5]}
+                                                                pt={[3, 3, 3, 3, 5]}
+                                                                pb={[3, 3, 3, 3, 5]}
+                                                                textAlign={[`left`, `left`, `center`, `center`, `center`]}
+                                                        // bgColor={`#0c3344`}
+                                                                alignItems={'center'}
+                                                                color={`#ffffff`}>
 
-                        {
-                            each?.submenu && each?.submenu.map(link => (
-                                <MenuItem  w={[`100%`,`100%`,`100%`,`fit-content`]} key={link?._id}><Link
-                                    href={`/${each?.category}/${link?.slug?.current}`}>{link?.title}</Link></MenuItem>))
-                        }
+                                                        <Link
+                                                            href={`/${each?.slug?.current}`}>{each?.title}</Link><ChevronDownIcon ml={2}/>
+                                                    </MenuButton>
+                                                    <MenuList>
+
+                                                        {
+                                                            each?.submenu && each?.submenu.map(link => (
+                                                                <MenuItem w={[`100%`, `100%`, `100%`, `fit-content`]}
+                                                                          key={link?._id}><Link
+                                                                    href={`/${each?.category}/${link?.slug?.current}`}>{link?.title}</Link></MenuItem>))
+                                                        }
 
 
-                    </MenuList>
-                </Menu>
+                                                    </MenuList>
+                                                </Menu>
 
 
+                                            )
+                                        })
+                                    }
 
-            )
-        })
-    }
+                                </Flex>
 
-</Flex>
+                            </Nav>
 
-</Nav>
                         </Navbar.Collapse>
+                        <Flex p={2}>
+                            <form onSubmit={(e) => {
+                                e.preventDefault()
+                                history.push({pathname: '/search', query: {text: text}})
+                            }} w={`auto`}>
+                                <InputGroup>
+                                    <InputLeftElement
 
-                </Navbar>
+                                        pointerEvents='none'
+                                        children={<SearchIcon color='#1e9339'/>}
+                                    />
+                                    <Input onChange={(e) => setText(e.target.value)} color={`#444444`} type='text'
+                                           bgColor={`#ffffff`} border={`solid`} borderColor={`#BC1F28`}
+                                           borderWidth={`1px`}/>
+                                </InputGroup>
 
+                            </form>
+                        </Flex>
+                    </Navbar>
 
-
-                    <Flex p={2} >
-                        <form  onSubmit={(e) => {
-                            e.preventDefault()
-                            history.push({pathname: '/search', query: {text: text}})
-                        }} w={`auto`} >
-                            <InputGroup >
-                                <InputLeftElement
-
-                                    pointerEvents='none'
-                                    children={<SearchIcon color='#1e9339'/>}
-                                />
-                                <Input onChange={(e) => setText(e.target.value)} color={`#444444`} type='text'
-                                       bgColor={`#ffffff`} border={`solid`} borderColor={`#BC1F28`}
-                                       borderWidth={`1px`}/>
-                            </InputGroup>
-
-                        </form>
-                    </Flex>
                 </Flex>
             </Container>
         </Flex>
