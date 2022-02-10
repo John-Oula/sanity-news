9283492066595167aa27e8c5149ecd4f67ee9ede
John-Oula <johnoula@icloud.com>
2022-02-11 05:36:31 +0800

    youtube window resize

--- sanity.js
+++ sanity.js
@@ -41,7 +41,10 @@ const serializers = {
         youtube: ({node}) => {
             const { url } = node
             const id = getYouTubeId(url)
-            return (<YouTube videoId={id} />)
+            return (<YouTube  opts = {{
+                height: 'fit-content',
+                width: 'auto',
+            }} videoId={id} />)
         },
         inlineVideo: ({node}) => {
 
