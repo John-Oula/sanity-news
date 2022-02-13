943178f51ed86c02127013c472c87bb3c3c90ef7
John-Oula <johnoula@icloud.com>
2022-02-13 19:52:57 +0800

    fix business directory wrapping

--- Components/CircleFilter.js
+++ Components/CircleFilter.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {Circle, Text, Flex, HStack} from "@chakra-ui/react";
+import {Circle, Text, Flex, Stack} from "@chakra-ui/react";
 
 function CircleFilter({state,data}) {
 
     const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     return (
-        <HStack mt={5} spacing={2}>
+        <Flex  display={['none','none','flex','flex','flex']} flexDirection={['column', 'row']} flexWrap={`wrap`} w={`auto`}>
             {
                 letters.map((each,index) =>{
                     return(
-                        <Circle p={2} key={index} color={`black`} size={`30px`}  bg={ state ? 'green' : 'gray.200'}>
+                        <Circle m={1} p={2} key={index} color={`black`} size={`30px`}  bg={ state ? 'green' : 'gray.200'}>
                         <Text>{each}</Text>
 
                     </Circle>)
                 })
             }
-        </HStack>
+        </Flex>
     );
 }
 
