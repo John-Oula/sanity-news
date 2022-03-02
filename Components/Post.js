bd735406abaeca61cfae7f2161d7a814df8350eb
John-Oula <johnoula@icloud.com>
2022-03-02 22:55:23 +0800

    image on job listing

--- Components/Post.js
+++ Components/Post.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Box, Button, Flex, Heading, Text} from "@chakra-ui/react";
+import {Box, Button, Flex, Heading, Text,Image} from "@chakra-ui/react";
 import Link from "next/link";
 import {TimeIcon} from "@chakra-ui/icons";
 import Moment from "react-moment";
-import {toPlainText} from "../sanity";
+import {toPlainText,urlFor} from "../sanity";
 import {useRouter} from 'next/router';
 
 function Post({data}) {
@@ -11,6 +11,11 @@ function Post({data}) {
     return (
         <Box borderBottom={`solid`} p={3} borderBottomWidth={`1px`} borderColor={` #d6d9dc`}
            >
+               <Flex>
+               <Box  mr={[0,0,4,4,4]} overflow={`hidden`} h={`inherit`} w={[`100%`,`100%`,`50%`,`50%`,`50%`]}  >
+                                                                        <Image fallbackSrc={`https://via.placeholder.com/200`} src={urlFor(data?.image).url()} w={`100%`} h={`auto`} />
+              </Box>
+            <Flex flexDirection={`column`}>
             <Heading color={`#287b4f`} mb={3} size={`sm`} as={`h5`}><Link href={`/post/${data.slug?.current}`}
                                                         passHref>{data?.title}</Link></Heading>
             <Flex alignItems={`center`}>
@@ -25,6 +30,8 @@ function Post({data}) {
             <Text>{toPlainText(data?.body)?.substring(0,300)}</Text>
             <Button mt={`10pt`} onClick={() => router.push(`/post/${data.slug?.current}`)} borderColor={` #d6d9dc`} variant='outline' borderRadius={`0px`} bgColor={`white`}>Read
                 More ...</Button>
+            </Flex>
+                </Flex>
         </Box>
     );
 }
