9cc3fb133d0e903ffbd4998245a6be22d7a71371
John-Oula <johnoula@icloud.com>
2022-03-07 22:18:37 +0800

    image alignment

--- Components/MediaContainer.js
+++ Components/MediaContainer.js
@@ -3,12 +3,24 @@ import { Box, Image } from "@chakra-ui/react";
 import { urlFor } from "../sanity";
 
 export default function MediaContainer({ url }) {
+    
     return (
        
-            <Image w={`100%`} h={`auto`} m={5} boxSize='20em'
+          <>
+           { 
+           url?.alignImage == 'left' || url?.alignImage == 'right' ? <Image className={url?.alignImage} w={`100%`} h={`auto`} m={5} boxSize='20em'
     objectFit='cover'
      fallbackSrc={`https://via.placeholder.com/200`}
                 src={urlFor(url)?.url()} />
+            :
+        
+        <Box m={5} overflow={`hidden`} h={300} w={`auto`}>
+            <Image className={url?.alignImage} w={`100%`} h={`auto`}  
+     fallbackSrc={`https://via.placeholder.com/200`}
+                src={urlFor(url)?.url()} />
+        </Box>
+        }
+          </>
       
     )
 }
